691f2d702a869f1391b536e8c3c7f06ad7829538
Dines Poulsen <dinespoulsen84@gmail.com>
2016-11-28 10:28:40 +0000

    few more feature tests

--- spec/features/navigating_spec.rb
+++ spec/features/navigating_spec.rb
@@ -32,11 +32,53 @@ RSpec.feature "RockPaperScissor", :type => :feature do
     expect(page).to have_text("Choose your weapon!")
   end
 
-  scenario "When I submit 'Rock' I am told if I have won" do
+  scenario "When user submit 'Rock' and opponent has 'scissors' user wins" do
     allow_any_instance_of(Array).to receive(:sample).and_return('Scissors')
     sign_in_and_play
     click_link("Rock")
     expect(page).to have_text "Dines wins"
+  end
+
+  scenario "When user submit 'Scissors' and opponent has 'Paper' user wins" do
+    allow_any_instance_of(Array).to receive(:sample).and_return('Paper')
+    sign_in_and_play
+    click_link("Scissors")
+    expect(page).to have_text "Dines wins"
+  end
+
+  scenario "When user submit 'Paper' and opponent has 'Rock' user wins" do
+    allow_any_instance_of(Array).to receive(:sample).and_return('Rock')
+    sign_in_and_play
+    click_link("Paper")
+    expect(page).to have_text "Dines wins"
+  end
+
+  scenario "When user submit 'Rock' and opponent has 'Paper' user looses" do
+    allow_any_instance_of(Array).to receive(:sample).and_return('Paper')
+    sign_in_and_play
+    click_link("Rock")
+    expect(page).to have_text "Master Gamer wins"
+  end
+
+  scenario "When user submit 'Paper' and opponent has 'Scissors' user looses" do
+    allow_any_instance_of(Array).to receive(:sample).and_return('Scissors')
+    sign_in_and_play
+    click_link("Paper")
+    expect(page).to have_text "Master Gamer wins"
+  end
+
+  scenario "When user submit 'Scissors' and opponent has 'Rock' user looses" do
+    allow_any_instance_of(Array).to receive(:sample).and_return('Rock')
+    sign_in_and_play
+    click_link("Scissors")
+    expect(page).to have_text "Master Gamer wins"
+  end
+
+  scenario "When user the same as the opponent it is a draw" do
+    allow_any_instance_of(Array).to receive(:sample).and_return('Rock')
+    sign_in_and_play
+    click_link("Rock")
+    expect(page).to have_text "The game was a draw"
   end
 
   scenario "When I win twice I am the winner of the game" do
@@ -46,5 +88,14 @@ RSpec.feature "RockPaperScissor", :type => :feature do
     click_link("Next Round")
     click_link("Rock")
     expect(page).to have_text "Wow you are a profet"
+  end
+
+  scenario "When I loose twice I am the looser of the game" do
+    allow_any_instance_of(Array).to receive(:sample).and_return('Scissors')
+    sign_in_and_play
+    click_link("Paper")
+    click_link("Next Round")
+    click_link("Paper")
+    expect(page).to have_text "That SUCKS Man"
   end
 end
